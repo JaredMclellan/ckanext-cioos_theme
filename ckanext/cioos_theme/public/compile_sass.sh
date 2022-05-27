36212e84aef8c417a1158a00b0fe04675878f753
JaredMclellan <jaredamclellan@gmail.com>
2022-05-27 13:55:11 -0300

    Added Vitality logos and css

--- ckanext/cioos_theme/public/compile_sass.sh
+++ ckanext/cioos_theme/public/compile_sass.sh
@@ -1,1 +1,1 @@
-sass --watch --poll --style=compressed cioos_atlantic.scss:cioos_atlantic.css cioos_theme.scss:cioos_theme.css
+sass --watch --poll --style=compressed cioos_atlantic.scss:cioos_atlantic.css cioos_theme.scss:cioos_theme.css cioos_vitality.scss:cioos_vitality.css
